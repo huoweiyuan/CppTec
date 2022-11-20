e5b02477937e8efc3aadb177be5a3b0c4d52f911
huoweiyuan <huoweiyuan@foxmail.com>
2022-11-20 14:17:59 +0800

    test singleton

---
+++ Test/Tool/singleton.cc
@@ -0,0 +1,39 @@
+#include "zy_singleton.h"
+#include <iostream>
+
+using namespace std;
+using namespace zy::tool;
+
+class A
+{
+ public:
+  void show() const {
+    cout << "A" << endl;
+  }
+};
+
+class B {
+ public:
+  void show() const {
+    cout << "B" << endl;
+  }
+};
+
+class C {
+ private:
+  char a[12];
+
+ public:
+  void show() const {
+    cout << "C " << &a << endl;
+    
+  }
+};
+
+int main(int argc, char **argv) {
+  Singleton<A>::getInstance()->show();
+  Singleton<B>::getInstance()->show();
+  Singleton<C>::getInstance()->show();
+  Singleton<C>::getInstance()->show();
+  return 0;
+}
