996f2115c31cfe7f659aaf22eb37955d0b2a3dde
huoweiyuan <huoweiyuan@foxmail.com>
2022-11-26 21:29:57 +0800

    test thread class

---
+++ Test/Thread/main.cc
@@ -0,0 +1,43 @@
+#include "Thread.h"
+#include <iostream>
+
+using namespace std;
+using namespace zy;
+
+class ThreadImpl : public Thread
+{
+ public:
+  ThreadImpl()
+    : Thread(false)
+  {
+  }
+
+  ~ThreadImpl() = default;
+
+  int thrdBegin(void) override
+  {
+    cout << "thrdBegin" << endl;
+    return 0;
+  }
+
+  int thrdEnd(void) override
+  {
+    cout << "thrdEnd" << endl;
+    return 0;
+  }
+
+  int thrdRoutine(void) override
+  {
+    cout << "thrdRoutine" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return 0;
+  }
+};
+
+int main(int argc, char **argv)
+{
+  ThreadImpl impl;
+  impl.start();
+  impl.join();
+  return 0;
+}
